fbcb2b6abc794b751b245813a329834d3e4429ad
Kevin <kevin.zawieja@haw-hamburg.de>
2023-04-26 18:20:40 +0200

    test

--- app/src/main/java/com/example/weeklyplaner/Add.java
+++ app/src/main/java/com/example/weeklyplaner/Add.java
@@ -17,7 +17,7 @@ public class Add extends AppCompatActivity {
         setContentView(R.layout.activity_add);
         imageButton = findViewById(R.id.imageButton);
         imageButton.setOnClickListener(v -> openBack());
-    }//haa
+    }//haa1
     public void openBack(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
