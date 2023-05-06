93be3533805f5e9e99038772b0738bd720a08b3d
Tugay <Tugayulas@outlook.de>
2023-05-06 16:14:00 +0200

    RegisterScreen BackButton hinzugefuegt

--- app/src/main/java/com/example/weeklyplaner/RegisterScreen.java
+++ app/src/main/java/com/example/weeklyplaner/RegisterScreen.java
@@ -2,13 +2,31 @@ package com.example.weeklyplaner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class RegisterScreen extends AppCompatActivity {
+
+   private ImageButton registerToLoginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+        registerToLoginButton = (ImageButton) findViewById(R.id.registerToLoginButton);
+
+        registerToLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterScreen.this,LoginScreen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
