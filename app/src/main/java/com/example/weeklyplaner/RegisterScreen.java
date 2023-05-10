1fd16c9a33630e6ac53292566d9ad7ed656b6c4d
ummar <ummar_j@outlook.com>
2023-05-11 00:24:53 +0200

    DB Connection successful :D

--- app/src/main/java/com/example/weeklyplaner/RegisterScreen.java
+++ app/src/main/java/com/example/weeklyplaner/RegisterScreen.java
@@ -5,11 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class RegisterScreen extends AppCompatActivity implements View.OnClickListener {
 
+    private TextView dbStatusTextView;
     private ImageButton backToLoginScreenButton;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private String email;
+    private String password;
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:mem:testdb";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,29 @@ public class RegisterScreen extends AppCompatActivity implements View.OnClickLis
         backToLoginScreenButton = findViewById(R.id.backToLoginScreenButton);
         backToLoginScreenButton.setOnClickListener(this);
 
+        editTextEmail = findViewById(R.id.editTextEmailAddress);
+
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        dbStatusTextView = findViewById(R.id.dbStatusTextView);
+
+
+        // Datenbank Verbindung
+        Connection connection = null;
+        try {
+            connection = getDBConnection();
+            dbStatusTextView.setText("You are successfully connected to your Database");
+        } catch (SQLException e) {
+            dbStatusTextView.setText("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                dbStatusTextView.setText("Error: " + e.getMessage());
+            }
+        }
     }
 
     @Override
@@ -30,5 +69,17 @@ public class RegisterScreen extends AppCompatActivity implements View.OnClickLis
             intent = new Intent(this, LoginScreen.class);
             startActivity(intent);
         }
+    }
+
+    public static Connection getDBConnection() throws SQLException {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            ;
+        }
+        connection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+        return connection;
     }
 }
