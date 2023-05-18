b91a4297db236c82231ec88eba954f77c6a73d7e
acq570 <marlon.misiak-hahnfeld@haw-hamburg.de>
2023-05-18 15:31:34 +0200

    code quality enhanced v1

--- app/src/main/java/com/example/weeklyplaner/MainActivity.java
+++ app/src/main/java/com/example/weeklyplaner/MainActivity.java
@@ -1,30 +1,31 @@
 package com.example.weeklyplaner;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.RelativeLayout;
-
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.TimeZone;
 
 import items.Termin;
-import items.TerminListe;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button button_for_days;
     private ImageButton addButton;
     private RecyclerView recyclerView;
+    private static ArrayList<Termin>[] terminListe = new ArrayList[7];
+    private Termin_RecyclerView_Adapter adapter;
+    private Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Berlin"));
     public static ArrayList<Termin> montag_terminliste = new ArrayList<>();
     public static ArrayList<Termin> dienstag_terminliste = new ArrayList<>();
     public static ArrayList<Termin> mittwoch_terminliste = new ArrayList<>();
@@ -32,46 +33,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static ArrayList<Termin> freitag_terminliste = new ArrayList<>();
     public static ArrayList<Termin> samstag_terminliste = new ArrayList<>();
     public static ArrayList<Termin> sonntag_terminliste = new ArrayList<>();
-    Termin_RecyclerView_Adapter adapter;
-    TimeZone german_timezone = TimeZone.getTimeZone("Europe/Berlin");
-    Calendar calendar = Calendar.getInstance(german_timezone);
+
 
     // REFRESH PAGE CODE
     @Override
     protected void onResume() {
         super.onResume();
-
-        // Refresh the activity here
         refreshMainActivity();
     }
 
     private void refreshMainActivity() {
-        // Refresh the specific day's data here
-        // For example, update the RecyclerView adapter with the latest data
-        if (calendar.get(Calendar.DAY_OF_WEEK) == 2) {
-            adapter = new Termin_RecyclerView_Adapter(this, MainActivity.montag_terminliste);
-            recyclerView.setAdapter(adapter);
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 3) {
-            adapter = new Termin_RecyclerView_Adapter(this, MainActivity.dienstag_terminliste);
-            recyclerView.setAdapter(adapter);
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 4) {
-            adapter = new Termin_RecyclerView_Adapter(this, MainActivity.mittwoch_terminliste);
-            recyclerView.setAdapter(adapter);
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 5) {
-            adapter = new Termin_RecyclerView_Adapter(this, MainActivity.donnerstag_terminliste);
-            recyclerView.setAdapter(adapter);
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 6) {
-            adapter = new Termin_RecyclerView_Adapter(this, MainActivity.freitag_terminliste);
-            recyclerView.setAdapter(adapter);
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 7) {
-            adapter = new Termin_RecyclerView_Adapter(this, MainActivity.samstag_terminliste);
-            recyclerView.setAdapter(adapter);
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 1) {
-            adapter = new Termin_RecyclerView_Adapter(this, MainActivity.sonntag_terminliste);
-            recyclerView.setAdapter(adapter);
-        }
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        adapter = new Termin_RecyclerView_Adapter(this, terminListe[dayOfWeek - 1]);
+        recyclerView.setAdapter(adapter);
     }
-//
 
     @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
@@ -84,58 +59,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         addButton = findViewById(R.id.AddButton);
         addButton.setOnClickListener(this);
-
-        // Log.v("HIERERERERER", "ARE WEEEEEEEEEEEEEEEEEEEEEEE");
-        //here
-
-        // setUpTermine();
         recyclerView = findViewById(R.id.TerminlisteRecyclerView);
 
-        if (calendar.get(Calendar.DAY_OF_WEEK) == 1) {
-            adapter = new Termin_RecyclerView_Adapter(this, sonntag_terminliste);
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 2) {
-            adapter = new Termin_RecyclerView_Adapter(this, montag_terminliste);
+        terminListe[0] = sonntag_terminliste;
+        terminListe[1] = montag_terminliste;
+        terminListe[2] = dienstag_terminliste;
+        terminListe[3] = mittwoch_terminliste;
+        terminListe[4] = donnerstag_terminliste;
+        terminListe[5] = freitag_terminliste;
+        terminListe[6] = samstag_terminliste;
 
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 3) {
-            adapter = new Termin_RecyclerView_Adapter(this, dienstag_terminliste);
-
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 4) {
-            adapter = new Termin_RecyclerView_Adapter(this, mittwoch_terminliste);
-
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 5) {
-            adapter = new Termin_RecyclerView_Adapter(this, donnerstag_terminliste);
-
-        } else if (calendar.get(Calendar.DAY_OF_WEEK) == 6) {
-            adapter = new Termin_RecyclerView_Adapter(this, freitag_terminliste);
-
-        } else {
-            adapter = new Termin_RecyclerView_Adapter(this, samstag_terminliste);
-
-        }
-        recyclerView.setAdapter(adapter);
-        // recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-
-        // Buttons generieren nach Erstellung
-        if (getIntent().getFlags() == getIntent().FLAG_GRANT_READ_URI_PERMISSION) {
-            int buttonCount = getIntent().getIntExtra("buttonCount", 0);
-            for (int i = 0; i < buttonCount; i++) {
-                Button newButton = new Button(this);
-                TerminListe terminListe = (getIntent().getSerializableExtra("terminliste", TerminListe.class));
-                newButton.setText(terminListe.getLastTermin().getTerminname());
-                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.WRAP_CONTENT,
-                        RelativeLayout.LayoutParams.WRAP_CONTENT
-                );
-                RelativeLayout relativeLayout = findViewById(R.id.MainActivityLayout);
-                params.addRule(RelativeLayout.BELOW, relativeLayout.getChildAt(i - 1).getId());
-                this.addContentView(newButton, params);
-            }
-        }
-
-
+        refreshMainActivity();
     }
+
 
     @Override
     public void onClick(View v) {
@@ -150,6 +87,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(intent);
         }
     }
+
 
 
 }
