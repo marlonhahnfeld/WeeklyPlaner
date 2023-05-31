3bb102c7197a2a7943971732f3052fed19608f50
Tugay Ulas <Tugay.ulas@haw-hamburg.de>
2023-05-31 19:00:51 +0200

    BUG fix for Terminchecked

--- app/src/main/java/com/example/weeklyplaner/Termin_RecyclerView_Adapter.java
+++ app/src/main/java/com/example/weeklyplaner/Termin_RecyclerView_Adapter.java
@@ -2,6 +2,7 @@ package com.example.weeklyplaner;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,8 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 
@@ -19,6 +22,9 @@ import items.Termin;
 
 public class Termin_RecyclerView_Adapter extends RecyclerView.Adapter<Termin_RecyclerView_Adapter.TerminViewHolder> {
     Context context;
+
+
+    CardView cardView;
     ArrayList<Termin> terminliste;
 
     public Termin_RecyclerView_Adapter(Context context, ArrayList<Termin> terminliste) {
@@ -59,6 +65,7 @@ public class Termin_RecyclerView_Adapter extends RecyclerView.Adapter<Termin_Rec
             TerminnameTextView = itemView.findViewById(R.id.TerminnameTextView);
             TerminPrioTextView = itemView.findViewById(R.id.TerminPrioritätTextView);
             checkbox = itemView.findViewById(R.id.checkbox);
+            cardView = itemView.findViewById(R.id.cardview);
 
             // Set click listener on the itemView
             itemView.setOnClickListener(this);
@@ -72,6 +79,16 @@ public class Termin_RecyclerView_Adapter extends RecyclerView.Adapter<Termin_Rec
                     if (position != RecyclerView.NO_POSITION) {
                         Termin termin = terminliste.get(position);
                         termin.setChecked(isChecked);
+
+                        if (isChecked) {
+                            cardView.setCardBackgroundColor(ContextCompat.getColor(context, R.color.gray));
+                            TerminnameTextView.setTextColor(Color.DKGRAY);
+                            TerminPrioTextView.setTextColor(Color.DKGRAY);
+                        } else {
+                            cardView.setCardBackgroundColor(ContextCompat.getColor(context, R.color.gray_Termin));
+                            TerminnameTextView.setTextColor(Color.WHITE);
+                            TerminPrioTextView.setTextColor(Color.WHITE);
+                        }
                     }
                 }
             });
