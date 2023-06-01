c638d4b68e1316d41df6552901872eefbaea83f6
ummar <ummar_j@outlook.com>
2023-06-01 02:17:40 +0200

    Test Commit

--- app/src/test/java/com/example/weeklyplaner/DatabaseOpTest.java
+++ app/src/test/java/com/example/weeklyplaner/DatabaseOpTest.java
@@ -1,84 +1,86 @@
-//  package com.example.weeklyplaner;
-
-// // import static org.junit.Assert.*;
-// // import static com.example.weeklyplaner.DatabaseOp.*;
-// // import static com.example.weeklyplaner.Utils.*;
-
-// // import org.junit.After;
-// // import org.junit.Before;
-// // import org.junit.Test;
-
-// // import java.sql.DatabaseMetaData;
-// // import java.sql.ResultSet;
-// // import java.sql.SQLException;
-// // import java.sql.Statement;
-
-//  public class DatabaseOpTest {
-// //     private static final String testAccount = "testAccount@hh.de";
-// //     private static final String testPassword = "Marlon123!";
-
-// //     @Before
-// //     public void setUp() {
-// //         createDatabaseConnection();
-// //     }
-
-// //     @Test
-// //     public void testCreateDatabaseConnection() {
-// //         assertTrue(isConnected());
-// //         closeDatabaseConnection();
-// //         assertFalse(isConnected());
-// //     }
-
-// //     @Test
-// //     public void testCloseDatabaseConnection() {
-// //         assertTrue(isConnected());
-// //         closeDatabaseConnection();
-// //         assertFalse(isConnected());
-// //     }
-
-// //     @Test
-// //     public void testDoesTableExists() throws SQLException {
-// //         assertTrue(doesTableExists());
-// //         if (isConnected()) {
-// //             DatabaseMetaData metaData = getConnection().getMetaData();
-// //             ResultSet resultSet = metaData.getTables(null, null,
-// //                     "NON_EXISTENT_TABLE", null);
-// //             boolean tableExists = resultSet.next();
-// //             resultSet.close();
-// //             assertFalse(tableExists);
-// //         }
-// //     }
-
-// //     @Test
-// //     public void testRegisterNewUser() throws SQLException {
-// //         Statement statement = getConnection().createStatement();
-// //         ResultSet resultSet;
-
-// //         String sqlQuery = "DELETE FROM TERMINE WHERE email = '" + testAccount + "';";
-// //         statement.execute(sqlQuery);
-// //         sqlQuery = "DELETE FROM LOGIN WHERE email = '" + testAccount + "';";
-// //         statement.execute(sqlQuery);
-
-// //         sqlQuery = "SELECT * FROM LOGIN WHERE email = '" + testAccount + "';";
-// //         resultSet = statement.executeQuery(sqlQuery);
-// //         assertFalse(resultSet.next());
-
-// //         registerNewUser(testAccount, testPassword);
-
-// //         sqlQuery = "SELECT * FROM LOGIN WHERE email = '" + testAccount + "';";
-// //         resultSet = statement.executeQuery(sqlQuery);
-// //         assertTrue(resultSet.next());
-
-// //         sqlQuery = "DELETE FROM TERMINE WHERE email = '" + testAccount + "';";
-// //         statement.execute(sqlQuery);
-// //         sqlQuery = "DELETE FROM LOGIN WHERE email = '" + testAccount + "';";
-// //         statement.execute(sqlQuery);
-
-// //         statement.close();
-// //         resultSet.close();
-// //     }
-// //     @After
-// //     public void closeUp() {
-// //         closeDatabaseConnection();
-// //     }
-//  }
+//package com.example.weeklyplaner;
+//
+//import static com.example.weeklyplaner.DatabaseOp.closeDatabaseConnection;
+//import static com.example.weeklyplaner.DatabaseOp.createDatabaseConnection;
+//import static com.example.weeklyplaner.DatabaseOp.createTables;
+//import static com.example.weeklyplaner.DatabaseOp.doesTableExists;
+//import static com.example.weeklyplaner.DatabaseOp.getConnection;
+//import static com.example.weeklyplaner.DatabaseOp.isConnected;
+//import static com.example.weeklyplaner.DatabaseOp.registerNewUser;
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertTrue;
+//
+//import org.junit.Test;
+//
+//import java.sql.DatabaseMetaData;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//
+//public class DatabaseOpTest {
+//    private static final String testAccount = "testAccount@hh.de";
+//    private static final String testPassword = "Marlon123!";
+//
+//    @Test
+//    public void testCreateDatabaseConnection() {
+//        createDatabaseConnection();
+//        assertTrue(isConnected());
+//        closeDatabaseConnection();
+//        assertFalse(isConnected());
+//    }
+//
+//    @Test
+//    public void testCloseDatabaseConnection() {
+//        createDatabaseConnection();
+//        assertTrue(isConnected());
+//        closeDatabaseConnection();
+//        assertFalse(isConnected());
+//    }
+//
+//    @Test
+//    public void testDoesTableExists() throws SQLException {
+//        createDatabaseConnection();
+//        createTables();
+//        assertTrue(doesTableExists());
+//        if (isConnected()) {
+//            DatabaseMetaData metaData = getConnection().getMetaData();
+//            ResultSet resultSet = metaData.getTables(null, null,
+//                    "NON_EXISTENT_TABLE", null);
+//            boolean tableExists = resultSet.next();
+//            resultSet.close();
+//            assertFalse(tableExists);
+//        }
+//        closeDatabaseConnection();
+//    }
+//
+//    @Test
+//    public void testRegisterNewUser() throws SQLException {
+//        createDatabaseConnection();
+//        Statement statement = getConnection().createStatement();
+//        ResultSet resultSet;
+//
+//        String sqlQuery = "DELETE FROM TERMINE WHERE email = '" + testAccount + "';";
+//        statement.execute(sqlQuery);
+//        sqlQuery = "DELETE FROM LOGIN WHERE email = '" + testAccount + "';";
+//        statement.execute(sqlQuery);
+//
+//        sqlQuery = "SELECT * FROM LOGIN WHERE email = '" + testAccount + "';";
+//        resultSet = statement.executeQuery(sqlQuery);
+//        assertFalse(resultSet.next());
+//
+//        registerNewUser(testAccount, testPassword);
+//
+//        sqlQuery = "SELECT * FROM LOGIN WHERE email = '" + testAccount + "';";
+//        resultSet = statement.executeQuery(sqlQuery);
+//        assertTrue(resultSet.next());
+//
+//        sqlQuery = "DELETE FROM TERMINE WHERE email = '" + testAccount + "';";
+//        statement.execute(sqlQuery);
+//        sqlQuery = "DELETE FROM LOGIN WHERE email = '" + testAccount + "';";
+//        statement.execute(sqlQuery);
+//
+//        statement.close();
+//        resultSet.close();
+//        closeDatabaseConnection();
+//    }
+//}
